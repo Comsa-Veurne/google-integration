eb341a88030d6286ad822d8b5768f16cedabd612
Ciryk Popeye <ciryk@hotmail.be>
2019-03-18 10:06:23 +0100

    Add oath2 example

--- src/Connector.php
+++ src/Connector.php
@@ -1,30 +1,112 @@
 <?php
 namespace Comsa\GoogleIntegration;
 
-/**
-*  A sample class
-*
-*  Use this section to define what this class is doing, the PHPDocumentator will use this
-*  to automatically generate an API documentation using this information.
-*
-*  @author yourname
-*/
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\GenericProvider;
+
 class Connector {
+    /**
+     * @var string
+     */
+    private $clientId;
 
-   /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-   private $m_SampleProperty = '';
+    /**
+     * @var string
+     */
+    private $clientSecret;
 
-  /**
-  * Sample method
-  *
-  * Always create a corresponding docblock for each method, describing what it is for,
-  * this helps the phpdocumentator to properly generator the documentation
-  *
-  * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
-  *
-  * @return string
-  */
-   public function method1($param1){
-			return "Hello World";
-   }
+    /**
+     * @var string
+     */
+    private $redirectUri;
+
+    /**
+     * @var string
+     */
+    private $urlAuthorize;
+
+    /**
+     * @var string
+     */
+    private $urlAccessToken;
+
+    /**
+     * @var string
+     */
+    private $urlResourceOwnerDetails;
+
+    /**
+     * Connector constructor.
+     * @param string $clientId
+     * @param string $clientSecret
+     * @param string $redirectUri
+     * @param string $urlAuthorize
+     * @param string $urlAccessToken
+     * @param string $urlResourceOwnerDetails
+     */
+    public function __construct(string $clientId, string $clientSecret, string $redirectUri, string $urlAuthorize, string $urlAccessToken, string $urlResourceOwnerDetails)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->redirectUri = $redirectUri;
+        $this->urlAuthorize = $urlAuthorize;
+        $this->urlAccessToken = $urlAccessToken;
+        $this->urlResourceOwnerDetails = $urlResourceOwnerDetails;
+    }
+
+    public function connect()
+    {
+        $provider = new GenericProvider([
+            'clientId' => $this->clientId,
+            'clientSecret' => $this->clientSecret,
+            'redirectUri' => $this->redirectUri,
+            'urlAuthorize' => $this->urlAuthorize,
+            'urlAccessToken' => $this->urlAccessToken,
+            'urlResourceOwnerDetails' => $this->urlResourceOwnerDetails
+        ]);
+
+        if (!isset($_GET['code'])) {
+            $authorizationUrl = $provider->getAuthorizationUrl();
+            $_SESSION['oauth2state'] = $provider->getState();
+            header('Location: ' . $authorizationUrl);
+            exit;
+        } elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+            if (isset($_SESSION['oauth2state'])) {
+                unset($_SESSION['oauth2state']);
+            }
+            exit('Invalid state');
+        } else {
+            try {
+                // Try to get an access token using the authorization code grant.
+                $accessToken = $provider->getAccessToken('authorization_code', [
+                    'code' => $_GET['code']
+                ]);
+
+                // We have an access token, which we may use in authenticated
+                // requests against the service provider's API.
+                echo 'Access Token: ' . $accessToken->getToken() . "<br>";
+                echo 'Refresh Token: ' . $accessToken->getRefreshToken() . "<br>";
+                echo 'Expired in: ' . $accessToken->getExpires() . "<br>";
+                echo 'Already expired? ' . ($accessToken->hasExpired() ? 'expired' : 'not expired') . "<br>";
+
+                // Using the access token, we may look up details about the
+                // resource owner.
+                $resourceOwner = $provider->getResourceOwner($accessToken);
+
+                var_export($resourceOwner->toArray());
+
+                // The provider provides a way to get an authenticated API request for
+                // the service, using the access token; it returns an object conforming
+                // to Psr\Http\Message\RequestInterface.
+                $request = $provider->getAuthenticatedRequest(
+                    'GET',
+                    'http://brentertainment.com/oauth2/lockdin/resource',
+                    $accessToken
+                );
+
+            } catch (IdentityProviderException $e) {
+                exit($e->getMessage());
+            }
+        }
+    }
 }
